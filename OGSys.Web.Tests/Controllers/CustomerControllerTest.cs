329e273cc879405ea41c8d20075e2fe31fc189f2
RTurek <rjturek@gmail.com>
2014-07-28 14:54:03 -0500

    squished home index, beginning test coverage

---
+++ OGSys.Web.Tests/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OGSys.Web;
+using OGSys.Web.Controllers;
+using OGSys.Web.Models;
+
+namespace OGSys.Web.Tests.Controllers
+{
+    [TestClass]
+    public class CustomerControllerTest
+    {
+        [TestMethod]
+        public void Customer_Index_Is_Not_Null()
+        {
+            // Arrange
+            CustomerController controller = new CustomerController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Customer_Details_Returns_A_Customer_Model()
+        {
+            // Arrange
+            CustomerController controller = new CustomerController();
+            Customer customer = new Customer();
+
+            // Act
+            ViewResult result = controller.Details(customer.CustomerId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- OGSys.Web.Tests/Controllers/HomeControllerTest.cs
+++ OGSys.Web.Tests/Controllers/HomeControllerTest.cs
@@ -13,39 +13,13 @@ namespace OGSys.Web.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void Index_Is_Not_Null()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
